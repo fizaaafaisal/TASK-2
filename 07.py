fa40a3abc9d2f026101de9c6575cc85f602c534f
fizaaafaisal <fathimafizafaisal@gmail.com>
2024-08-20 20:40:58 +0530

    07.py

---
+++ 07.py
@@ -0,0 +1,9 @@
+#positive int entered by the user
+
+while(True):
+        n=int(input("enter a number:"))
+        i=0
+        while(i<n):
+                i+=1
+                if(n%i==0):
+                        print(i)
