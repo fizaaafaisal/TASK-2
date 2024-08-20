32320ec552dca4b0bd246ab9f0ebcc6d4f7eacc9
fizaaafaisal <fathimafizafaisal@gmail.com>
2024-08-20 20:36:28 +0530

    04.py

---
+++ 04.py
@@ -0,0 +1,9 @@
+#password
+
+while(True):
+        password=input("enter the password:")
+        if(password=="Nakkiii"):
+                            print("correct password")
+        if(password=="quit"):
+                            break
+
